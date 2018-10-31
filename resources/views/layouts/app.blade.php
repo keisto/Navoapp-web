c5f51720af8e19f0c55f0f5e3b24d0fbd52deb04
Tony Keiser <keisertony@gmail.com>
2018-10-31 10:57:19 -0600

    Website Description

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no, user-scalable=no">
+    <meta name="description" content="Oil & Gas Well Locator. Advanced search made simple.">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
