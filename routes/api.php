ba08b636fbb45d220e3214462c55cf2497d2a913
Tony Keiser <keisertony@gmail.com>
2019-04-01 15:20:10 -0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,23 @@ use Illuminate\Http\Request;
 Route::post('login', 'API\UserController@login');
 Route::post('register', 'API\UserController@register');
 
+Route::get('ephemeral', function () {
+    if (!isset($_POST['api_version'])) {
+        exit(http_response_code(400));
+    }
+    try {
+        $key = \Stripe\EphemeralKey::create(
+//            ["customer" => $customerId],
+            ["stripe_version" => $_POST['api_version']]
+        );
+        header('Content-Type: application/json');
+        exit(json_encode($key));
+    } catch (Exception $e) {
+        exit(http_response_code(500));
+    }
+});
+
+
 Route::group(['middleware' => 'auth:api'], function(){
     Route::post('details', 'API\UserController@details');
     Route::post('logout', 'API\UserController@logout');
